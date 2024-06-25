46e45cdbef1c03e4afef0527836bbdab2e677606
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-26 02:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+let x = prompt("Enter the first number:");
+let y = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+if(typeof(x)==number && typeof(y)==number){
+	alert(x+y);
+}
+else{
+	alert("Invalid input. Please enter a valid number.")
+}
